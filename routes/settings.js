18c446fa4c2c912d71225da8652dbcbc83ab6cb0
Uncle CHEN <114413511@qq.com>
2016-02-18 14:51:10 +0800

    fix

--- routes/settings.js
+++ routes/settings.js
@@ -1,8 +1,7 @@
 module.exports = {
-		host: '117.121.25.131',
-        /*host: '127.0.0.1',*/
+        host: '127.0.0.1',
         user: 'root',
-        password: 'password123',
+        password: 'password',
         database:'aiship',
         port: 3306,
         imgurl: 'http://7xr0h8.com1.z0.glb.clouddn.com//aiyouchuan_',
