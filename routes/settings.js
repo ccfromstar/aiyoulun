dd2c5b73273ad5ee7a9bf2889461904296285459
ccfromstar <114413511@qq.com>
2016-03-15 15:16:54 +0800

    2016-3-15 update

--- routes/settings.js
+++ routes/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
-        host: '127.0.0.1',
+        host: '117.121.25.131',
         user: 'root',
-        password: 'password',
+        password: 'password123',
         database:'aiship',
         port: 3306,
         imgurl: 'http://7xr0h8.com1.z0.glb.clouddn.com//aiyouchuan_',
