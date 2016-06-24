f9bc8dc408750ea0e164f31a09d0efbd0ec7d70c
ccfromstar <114413511@qq.com>
2016-06-24 15:53:29 +0800

    update

--- routes/settings.js
+++ routes/settings.js
@@ -1,8 +1,8 @@
 module.exports = {
-        host: '117.121.25.131',
-        user: 'root',
-        password: 'password123',
-        database:'aiship',
+        host: '',
+        user: '',
+        password: '',
+        database:'',
         port: 3306,
-        imgurl: 'http://7xr0h8.com1.z0.glb.clouddn.com//aiyouchuan_',
+        imgurl: '',
 };
